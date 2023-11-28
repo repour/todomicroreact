3d01eeb2b9dd6ec12ebc1aba5dbd6dd650d418e5
repour <poorsadeghi.r@gmail.com>
2023-11-29 03:05:22 +0330

    compelete arrow edited

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -18,7 +18,7 @@ function Icon({ id, open }) {
             viewBox="0 0 24 24"
             strokeWidth={2}
             stroke="currentColor"
-            className={`${id === open ? "rotate-0" : ""} rotate-180 h-5 w-5 transition-transform`}
+            className={`${id === open ? "rotate-0" : "-rotate-90"} text-gray-500 h-5 w-5 transition-transform`}
         >
             <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
         </svg>
@@ -138,7 +138,7 @@ const TodoList = (props) => {
                         compeleteTodo.length
                             ?
                             <>
-                                <div className='flex gap-3 m-4 text' onClick={() => handleOpen(1)}>
+                                <div className='flex items-center gap-3 m-4' onClick={() => handleOpen(1)}>
                                     <Icon id={1} open={open} />
                                     compeleted <span className='text-gray-500'>{compeleteTodo.length}</span>
                                 </div>
